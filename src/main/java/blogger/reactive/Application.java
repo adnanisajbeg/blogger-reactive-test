87e2f1cccb3a6b8b620159c8ab5b79f38ebe3fb4
adnan.isajbegovic <adnan.isajbegovic@hpe.com>
2018-03-17 19:22:05 +0100

    init commit

---
+++ src/main/java/blogger/reactive/Application.java
@@ -0,0 +1,16 @@
+package blogger.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Adnan Isajbegovic
+ * @Created: 2018-03-17
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
